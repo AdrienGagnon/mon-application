f2b47b0486dd30c104f2853e4c557afed6083263
Adrien Gagnon <118489764+AdrienGagnon@users.noreply.github.com>
2023-03-08 19:41:55 -0500

    adding scroll to

--- src/js/main.js
+++ src/js/main.js
@@ -57,7 +57,7 @@ let counter = 0;
 function animate() {
     requestAnimationFrame(animate);
     counter += 0.005;
-    sphere.rotation.y += 0.002;
+    sphere.rotation.y += 0.005;
     sphere.rotation.x = Math.sin(counter) / 2;
     renderer.render(scene, camera);
 }
@@ -239,3 +239,34 @@ const observeSection3 = function (section) {
 
 observeSection1(section1);
 observeSection3(section3);
+
+///////////////////////////////////////////////////
+// Scrolling to sections
+const toInfos = document.querySelector('.to-section-infos');
+const toPhotos = document.querySelector('.to-section-photos');
+const toQuiz = document.querySelector('.to-section-quiz');
+const toTop = document.querySelector('.revenirTop');
+
+toInfos.addEventListener('click', function (e) {
+    section1.scrollIntoView({
+        behavior: 'smooth',
+    });
+});
+
+toPhotos.addEventListener('click', function (e) {
+    section2.scrollIntoView({
+        behavior: 'smooth',
+    });
+});
+
+toQuiz.addEventListener('click', function (e) {
+    section3.scrollIntoView({
+        behavior: 'smooth',
+    });
+});
+
+toTop.addEventListener('click', function (e) {
+    header.scrollIntoView({
+        behavior: 'smooth',
+    });
+});
