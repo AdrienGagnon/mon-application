3a87e45e7e67114d94ce02f89837058a51281565
Adrien Gagnon <118489764+AdrienGagnon@users.noreply.github.com>
2023-03-17 18:04:11 -0400

    images in preview

---
+++ src/img/photos-page/index.js
@@ -0,0 +1,60 @@
+import img0 from './amsterdam-canal.jpg';
+import img1 from './Corniglia-maisons.jpg';
+import img2 from './Florence-Santa-Maria-del-Fiore.jpg';
+import img3 from './paris-eiffel.jpg';
+import img4 from './Tre-Cime-Di-Lavaredo-north.jpg';
+import img5 from './Corniglia-maisons.jpg';
+import img6 from './Corniglia-maisons.jpg';
+
+export const imageID = [
+    {
+        id: 0,
+        pays: 'Pays-Bas',
+        ville: 'Amsterdam',
+        description: 'Un magnifique canal en plein coeur de la capitale',
+        imgName: 'amsterdam-canal',
+        coords: [52.37185989346697, 4.895886074989968],
+    },
+    {
+        id: 1,
+        pays: 'Italie',
+        ville: 'Corniglia',
+        description: 'Les maisons colorées au bord de la mer Ligure',
+        imgName: 'Corniglia-maisons.jpg',
+        coords: [44.121088333127155, 9.70801465455592],
+    },
+    {
+        id: 2,
+        pays: 'Italie',
+        ville: 'Florence',
+        description: 'La cathédrale médiévale au coeur de la ville',
+        imgName: 'Florence-Santa-Maria-del-Fiore.jpg',
+        coords: [43.77271987812075, 11.255739566568433],
+    },
+    {
+        id: 3,
+        pays: 'France',
+        ville: 'Paris',
+        description: 'La tour Eiffel',
+        imgName: 'paris-eiffel.jpg',
+        coords: [52.37185989346697, 4.895886074989968],
+    },
+    {
+        id: 4,
+        pays: 'Italie',
+        ville: 'Tre Cime di Lavaredo',
+        description: 'Situé dans les Alpes italliennes',
+        imgName: 'Tre-Cime-Di-Lavaredo-north.jpg',
+        coords: [52.37185989346697, 4.895886074989968],
+    },
+    {
+        id: 4,
+        pays: 'Italie',
+        ville: 'Tre Cime di Lavaredo',
+        description: 'Situé dans les Alpes italliennes',
+        imgName: 'Tre-Cime-Di-Lavaredo-north.jpg',
+        coords: [52.37185989346697, 4.895886074989968],
+    },
+];
+
+export const photosArray = [img0, img1, img2, img3, img4, img5, img6];
--- src/jsx/Imagespreview.jsx
+++ src/jsx/Imagespreview.jsx
@@ -1,33 +1,35 @@
 'use strict';
 
-import { imageID } from './photo-id';
-
+import { imageID } from '../img/photos-page';
+import { photosArray } from '../img/photos-page';
+console.log(imageID);
 export default function Imagespreview() {
+    // require(`../img/photos-page/amsterdam-canal.jpg`)
     return (
-        <div>
-            {imageID.forEach(function (image) {
-                const refImage = '../img/photos-page/';
-
-                return (
-                    <div className="photo-container">
-                        <img
-                            className="img-preview"
-                            src={refImage + image.imgName}
-                        />
-                        <div className="img-description">
-                            <div className="img-lieu">
-                                <img
-                                    className="icon-photo"
-                                    src={require('../img/photos-page/icon-photo.png')}
-                                    alt=""
-                                />
-                                {image.ville}, {image.pays}
+        <>
+            {
+                <>
+                    {imageID.map(function (image) {
+                        const img = photosArray[image.id];
+                        return (
+                            <div key={image.id} className="photo-container">
+                                <img className="img-preview" src={img} />
+                                <div className="img-description">
+                                    <div className="img-lieu">
+                                        <img
+                                            className="icon-photo"
+                                            src={require('../img/photos-page/icon-photo.png')}
+                                            alt=""
+                                        />
+                                        {image.ville}, {image.pays}
+                                    </div>
+                                    {image.description}
+                                </div>
                             </div>
-                            {image.description}
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
+                        );
+                    })}
+                </>
+            }
+        </>
     );
 }
